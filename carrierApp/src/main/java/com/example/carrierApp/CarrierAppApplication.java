913045a764dabc095edc3d9427bc3bb5aeba4139
pdalelopez <pdalelopez@users.noreply.github.com>
2020-06-03 11:03:32 +0200

    Project created.

---
+++ carrierApp/src/main/java/com/example/carrierApp/CarrierAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrierApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrierAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrierAppApplication.class, args);
+	}
+
+}
---
+++ carrierApp/src/test/java/com/example/carrierApp/CarrierAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carrierApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrierAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
